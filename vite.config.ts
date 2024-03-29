34a3b021ad49326b5d75c27e5e9f3f201cb8266c
SZSage <simonz@uoregon.edu>
2024-03-28 20:56:18 -0700

    changed path

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/",
+	base: "./",
 	plugins: [react()],
-
-	build: {
-		manifest: true,
-		rollupOptions: {
-			input: "src/main.tsx"
-		}
-	}
 })
