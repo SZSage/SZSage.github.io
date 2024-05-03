d10999fbefd2f0c25b2f00bb989393b606a58ea1
SZSage <simonz@uoregon.edu>
2024-05-02 18:28:39 -0700

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/SZSage.github.io/",
 	plugins: [react()],
 
 	build: {
