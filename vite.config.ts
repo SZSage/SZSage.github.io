f9cdc7087c9bd3cc8cdff4ba39620ae677e3f56a
SZSage <simonz@uoregon.edu>
2024-05-02 19:47:01 -0700

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "./",
-	plugins: [react()],
-
-	build: {
-		manifest: true,
-		rollupOptions: {
-			input: 'src/main.tsx'
-		}
-    }
+	plugins: [react()]
 });
